140ae0b7575e06d28b1672366f083327d8fbee51
lucas <mcbride.lucas9@gmail.com>
2016-09-30 00:22:30 -0400

    username cannot be ''

--- app.js
+++ app.js
@@ -274,8 +274,9 @@ var isValidPassword = function(data,cb){
 }
 var isUsernameTaken = function(data,cb){
 	// return cb(false);
-	db.account.find({username:data.username},function(err,res){
-		if(res.length > 0)
+
+			db.account.find({username:data.username},function(err,res){
+		if(res.length > 0 && data.username != '')
 			cb(true);
 		else
 			cb(false);
